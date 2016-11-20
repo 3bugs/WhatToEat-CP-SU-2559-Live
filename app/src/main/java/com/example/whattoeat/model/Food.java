9f490e68807c34bba89abcd8b16e83264ccacf89
Promlert Lovichit <promlert@gmail.com>
2016-11-20 10:05:48 +0700

    Add class Food

---
+++ app/src/main/java/com/example/whattoeat/model/Food.java
@@ -0,0 +1,16 @@
+package com.example.whattoeat.model;
+
+/**
+ * Created by Promlert on 2016-11-20.
+ */
+
+public class Food {
+
+    public final String name;
+    public final String pictureFilename;
+
+    public Food(String name, String pictureFilename) {
+        this.name = name;
+        this.pictureFilename = pictureFilename;
+    }
+}
