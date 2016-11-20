abe3c637bb3cc79ce34eac2a11b791109da92a22
Promlert Lovichit <promlert@gmail.com>
2016-11-20 11:37:29 +0700

    Update class FoodMenu and MainActivity

--- app/src/main/java/com/example/whattoeat/model/FoodMenu.java
+++ app/src/main/java/com/example/whattoeat/model/FoodMenu.java
@@ -1,6 +1,14 @@
 package com.example.whattoeat.model;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.util.Log;
+
+import com.example.whattoeat.db.DatabaseHelper;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Created by Promlert on 2016-11-20.
@@ -8,8 +16,16 @@ import android.content.Context;
 
 public class FoodMenu {
 
+    private static final String TAG = FoodMenu.class.getSimpleName();
+
     private static FoodMenu mInstance;
     private Context mContext;
+
+    private DatabaseHelper mHelper;
+    private SQLiteDatabase mDb;
+
+    private ArrayList<Food> mFoodList = new ArrayList<>();
+    private Random mRandom = new Random();
 
     public static FoodMenu getInstance(Context context) {
         if (mInstance == null) {
@@ -20,5 +36,37 @@ public class FoodMenu {
 
     private FoodMenu(Context context) {
         this.mContext = context;
+    }
+
+    public void loadFromDatabase() {
+        mFoodList.clear();
+
+        mHelper = new DatabaseHelper(mContext);
+        mDb = mHelper.getWritableDatabase();
+
+        Cursor cursor = mDb.query(DatabaseHelper.TABLE_NAME, null, null, null, null, null, null);
+
+        while (cursor.moveToNext()) {
+            String name = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_NAME));
+            String picture = cursor.getString(cursor.getColumnIndex(DatabaseHelper.COL_PICTURE));
+
+            Food food = new Food(name, picture);
+            mFoodList.add(food);
+        }
+        cursor.close();
+        //mDb.close();
+        //mHelper.close();
+    }
+
+    public ArrayList<Food> getFoodList() {
+        return mFoodList;
+    }
+
+    public Food getRandomFood() {
+        int randomIndex = mRandom.nextInt(mFoodList.size());
+        Food food = mFoodList.get(randomIndex);
+
+        Log.i(TAG, food.name + ", " + food.pictureFilename);
+        return food;
     }
 }
